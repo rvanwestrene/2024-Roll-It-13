eaf0f4a202c555065ed6f903928e3e6e83dc478e
rvanwestrene <159123031+rvanwestrene@users.noreply.github.com>
2024-02-21 14:11:46 +1300

    Updated Dice sim (added double roll + added score)

--- C_04_Dice_Simulator.py
+++ C_04_Dice_Simulator.py
@@ -1,5 +1,26 @@
 import random
 
-for item in range(0, 10):
-    dice_roll = random.randint(1, 3)
-    print(dice_roll, end="\t")
+
+def roll_die():
+    result = random.randint(1, 6)
+    return result
+
+
+# Main routine starts here
+user_score = 0
+double_score = "no"
+
+# roll 2 dice
+roll_1 = roll_die()
+roll_2 = roll_die()
+
+# check if we have a double score opportunity
+if roll_1 == roll_2:
+    double_score = "yes"
+
+# find the total points (so far)
+user_points = roll_1 + roll_2
+
+# show the user the result
+print(f"Die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+print(f"Double score opportunity: {double_score}")
